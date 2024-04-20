43e2f33977aedff171cea9c1ec72dbf023b28aa5
GemmaTop <118997593+GemmaTop@users.noreply.github.com>
2024-04-21 00:39:30 +0100

    added all woods screens

---
+++ app/src/main/java/com/example/developmentproject_p2673488/FinishedLevelWoods.kt
@@ -0,0 +1,11 @@
+package com.example.developmentproject_p2673488
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FinishedLevelWoods : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_finished_level_woods)
+    }
+}
---
+++ app/src/main/java/com/example/developmentproject_p2673488/WoodsInstructions.kt
@@ -0,0 +1,11 @@
+package com.example.developmentproject_p2673488
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WoodsInstructions : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_woods_instructions)
+    }
+}
