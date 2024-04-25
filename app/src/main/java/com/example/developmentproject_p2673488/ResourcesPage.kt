f601fe1095270d430f4bec54bcf00712182ff3d1
GemmaTop <118997593+GemmaTop@users.noreply.github.com>
2024-04-25 13:12:38 +0100

    added links to resources page

--- app/src/main/java/com/example/developmentproject_p2673488/ResourcesPage.kt
+++ app/src/main/java/com/example/developmentproject_p2673488/ResourcesPage.kt
@@ -1,6 +1,7 @@
 package com.example.developmentproject_p2673488
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -15,4 +16,20 @@ class ResourcesPage : AppCompatActivity() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
+
+    fun litterCritterBtn (view : View){
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.abcya.com/games/recycling_game"))
+        startActivity(intent)
+    }
+
+    fun bugHotelsBtn (view : View){
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.famly.co/blog/bug-hotels-early-years"))
+        startActivity(intent)
+    }
+
+    fun songBtn (view : View){
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.chosic.com/download-audio/58134/#google_vignette"))
+        startActivity(intent)
+    }
+
 }
